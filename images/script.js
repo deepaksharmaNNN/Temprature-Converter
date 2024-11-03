18d0e6461237598287a33888333e5dd9937c7295
Deepak Sharma <dsharma2828@gmail.com>
2024-11-04 00:05:45 +0530

    Init

---
+++ images/script.js
@@ -0,0 +1,4 @@
+document.getElementById("convert").onclick = function() {
+    let temp = parseFloat(document.getElementById("temp").value);
+    let unit = document.getElementById("unit").value;
+}
